b921b45547384d3e1b7a294b134ac12dbfdc222c
Sabelo Gumede <iamsabelogumede@gmail.com>
2018-10-31 15:22:11 +0200

    Middleware: body-parser, express-session, validator, messages, connect-flash

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
 var config = require('./config/database');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var ExpressValidator = require('express-validator');
+
 
 // connect to db
 mongoose.connect(config.database);
@@ -22,6 +26,48 @@ app.set('view engine', 'ejs');
 // set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Body Parser middleware
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+// Express Session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
+// Express Validator middleware
+app.use(ExpressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root = namespace.shift()
+        , formParam = root;
+
+    while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+        param : formParam,
+        msg : msg,
+        value : value
+      };
+    }
+}));
+
+// Express messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 // Set routes
 var pages = require('./routes/pages.js');
