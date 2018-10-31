941aca1a50a5a4434e5c765f9e28740e8a4532d4
Sabelo Gumede <iamsabelogumede@gmail.com>
2018-10-31 16:28:52 +0200

    get add page

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-// get route
+// Get pages index
 router.get('/', function(req, res) {
     res.send('admin area');
 });
 
+// Get add pages
+router.get('/add-page', function(req, res) {
 
+    var title = "";
+    var slug = "";
+    var content = "";
+
+    res.render('admin/add_page', {
+        title: title,
+        slug: slug,
+        content: content
+    });
+
+});
 
 // Exports
 module.exports = router;
